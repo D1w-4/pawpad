2a80b3f13e13cb9481be85a18da2cb445ed9efcc
Муравьев Артем Андреевич <muravev_a@tochka.com>
2019-12-02 12:33:43 +0500

    add matireal ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <Button color={'secondary'}> s</Button>
       </header>
     </div>
   );
